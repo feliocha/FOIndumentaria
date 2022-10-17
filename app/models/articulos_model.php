179a6c76337572f81042c9f48b3e9f21bf0dc36b
feliocha <ochandorenaf@gmail.com>
2022-10-17 19:20:39 -0300

    Primer commit

---
+++ app/models/articulos_model.php
@@ -0,0 +1,59 @@
+<?php
+
+class ArticulosModel {
+
+    private function getDB() {
+        $db = new PDO('mysql:host=localhost;'.'dbname=FOIndumentaria;charset=utf8', 'root', '');
+        return $db;
+    }
+    
+    function getArticulos() {
+        $db = $this->getDB();
+        $query = $db->prepare("SELECT * FROM articulos");
+        $query->execute();
+        $articulos = $query->fetchAll(PDO::FETCH_OBJ);
+        return $articulos;
+    }
+    
+    function getArticulosPorCat($id_categoria) {
+        $db = $this->getDB();
+        $query = $db->prepare("SELECT * FROM articulos WHERE id_categoria = ?");
+        $query->execute([$id_categoria]);
+        $articulos = $query->fetchAll(PDO::FETCH_OBJ);
+        return $articulos;
+    }
+
+    function getArticulo($id) {
+        $db = $this->getDB();
+        $query = $db->prepare("SELECT * FROM articulos WHERE id = ?");
+        $query->execute([$id]);
+        $articulo = $query->fetch(PDO::FETCH_OBJ);
+        return $articulo;
+    }
+
+    function insertArticulo($nombre, $precio, $id_categoria) {
+        $db = $this->getDB();
+        $query = $db->prepare("INSERT INTO articulos (nombre, precio, id_categoria) VALUES (?, ?, ?)");
+        $query->execute([$nombre, $precio, $id_categoria]);
+        return $db->lastInsertId();
+    }
+    
+    function deleteArticulo($id) {
+        $db = $this->getDB();
+        $query = $db->prepare("DELETE FROM articulos WHERE id = ?");
+        $query->execute([$id]);
+    }
+
+    function updateArticulo($nombre,$precio,$id) {
+        $db = $this->getDB();
+        $query = $db->prepare("UPDATE articulos SET nombre=?, precio=? WHERE id=?");
+        $query->execute([$nombre,$precio,$id]);
+    }
+
+}
+
+
+
+
+
+?>
---
+++ templates/categoriaEspecifica.tpl
@@ -0,0 +1,17 @@
+{include file="header.tpl"}
+
+<h1> {$categoria->nombre} </h1>
+
+<h3> Articulos en la categoria: </h3>
+<ul class="list-group">
+    {foreach from=$articulos item=$articulo}
+        <li class='list-group-item d-flex justify-content-between align-items-center'>
+            <span href='showArticulo/{$articulo->id}'> <a href='showArticulo/{$articulo->id}'>{$articulo->nombre}</a> : {$articulo->precio}</span>
+            <a href='deleteArticulo/{$articulo->id}' type='button' class='btn btn-danger ml-auto'>Borrar</a>
+        </li>
+    {/foreach}
+</ul>
+
+
+
+{include file="footer.tpl"}
